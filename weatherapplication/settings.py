2ac1ed1ed8003bb8d3e27a99e08308ea24f6816c
javad-1990 <j.borhani1369@gmail.com>
2023-06-17 19:43:09 +0430

    'final'

--- weatherapplication/settings.py
+++ weatherapplication/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-q-%gbkm^+(lp(wch5#eghrnpj^kyr0xe!x$b+k@wf$u7$y6s6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- weatherupdates/views.py
+++ weatherupdates/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
+from datetime import datetime
 
 # Create your views here.
+# the index() will handle all the app's logic
+def index(request):
+    try:
+        if request.method == 'POST':
+            API_KEY = 'b36339f5329379c9fc44ad0583e4ed5d'
+            city_name = request.POST.get('city')
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
+            response = requests.get(url).json()
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
+            city_weather_update = {
+                'city': city_name,
+                'description': response['weather'][0]['description'],
+                'icon': response['weather'][0]['icon'],
+                'temperature': 'Temperature: ' + str(response['main']['temp']) + ' °C',
+                'country_code': response['sys']['country'],
+                'wind': 'Wind: ' + str(response['wind']['speed']) + 'km/h',
+                'humidity': 'Humidity: ' + str(response['main']['humidity']) + '%',
+                'time': formatted_time
+            }
+        else:
+            city_weather_update = {}
+        context = {'city_weather_update': city_weather_update}
+        return render(request, 'weatherupdates/home.html', context)
+        print(city_weather_update)
+    except:
+        return render(request, 'weatherupdates/404.html')
+
